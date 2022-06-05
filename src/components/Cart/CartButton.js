029d2336d61a6b814f1cbd45578813625d8ea65f
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-06 01:48:15 +0500

    setting up a store & toggling cart visibility

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,8 +1,16 @@
+import { useDispatch, useSelector } from 'react-redux';
+import { toggle } from '../../store/ui-slice';
 import classes from './CartButton.module.scss';
 
 export default function CartButton(props) {
+  const dispatch = useDispatch();
+
+  const toggleCartHandler = () => {
+    dispatch(toggle());
+  };
+
   return (
-    <button className={classes.button}>
+    <button className={classes.button} onClick={toggleCartHandler}>
       <span>My Cart</span>
       <span className={classes.badge}>1</span>
     </button>
