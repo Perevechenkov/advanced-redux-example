3cbe8406550749d62ff6f58e5a3a3f9ebbca2bbc
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-07 00:42:11 +0500

    sending put req in the useEffect (however empty cart will overwrite cart in the firebase after firtst render)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
+import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 
 function App() {
-  const isVisible = useSelector(state => state.ui.cartIsVisible);
+  const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch(
+      'https://movies-dummy-db-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+      { method: 'PUT', body: JSON.stringify(cart) }
+    );
+  }, [cart]);
 
   return (
     <Layout>
-      {isVisible && <Cart />}
+      {cartIsVisible && <Cart />}
       <Products />
     </Layout>
   );
