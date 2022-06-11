1bce9d021fbdbc8cb230bc56827881b11a0e1544
DesperateHiking <perevechenkov_1999@mail.ru>
2022-06-11 22:09:21 +0500

    showing notification w/ request response

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
 import { useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import Notification from './components/UI/Notification';
+import { showNotification } from './store/ui-slice';
 
 function App() {
   const cartIsVisible = useSelector(state => state.ui.cartIsVisible);
   const cart = useSelector(state => state.cart);
+  const notification = useSelector(state => state.ui.notification);
+
+  const dispatch = useDispatch();
 
   useEffect(() => {
-    fetch(
-      'https://movies-dummy-db-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
-      { method: 'PUT', body: JSON.stringify(cart) }
-    );
-  }, [cart]);
+    const sendCartData = async () => {
+      dispatch(
+        showNotification({
+          status: 'pending',
+          title: 'Sending',
+          message: 'sending cart data',
+        })
+      );
+
+      const response = await fetch(
+        'https://movies-dummy-db-default-rtdb.europe-west1.firebasedatabase.app/cart.json',
+        { method: 'PUT', body: JSON.stringify(cart) }
+      );
+
+      if (!response.ok) {
+        throw new Error(response.statusText);
+      }
+
+      dispatch(
+        showNotification({
+          status: 'success',
+          title: 'Success',
+          message: 'Sent cart data successfully',
+        })
+      );
+    };
+
+    sendCartData().catch(err => {
+      dispatch(
+        showNotification({
+          status: 'error',
+          title: err.message,
+          message: 'Sending cart data failed',
+        })
+      );
+    });
+  }, [cart, dispatch]);
 
   return (
-    <Layout>
-      {cartIsVisible && <Cart />}
-      <Products />
-    </Layout>
+    <>
+      {notification && <Notification item={notification} />}
+      <Layout>
+        {cartIsVisible && <Cart />}
+        <Products />
+      </Layout>
+    </>
   );
 }
 
---
+++ src/components/UI/Notification.js
@@ -0,0 +1,19 @@
+import classes from './Notification.module.scss';
+
+export default function Notification(props) {
+  const { status, title, message } = props.item;
+
+  let specialClasses = '';
+
+  if (status === 'error') specialClasses = classes.error;
+  if (status === 'success') specialClasses = classes.success;
+
+  const scssClasses = `${classes.notification} ${specialClasses}`;
+  console.log(specialClasses);
+  return (
+    <section className={scssClasses}>
+      <h2>{title}</h2>
+      <p>{message}</p>
+    </section>
+  );
+}
